f856a96563ea4aede6a851595205ac92b4dfd69c
suminpark123 <a55026851@gmail.com>
2023-12-20 11:36:26 +0900

    connect database

---
+++ src/test/java/com/sumin/spring_crud/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumin.spring_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
