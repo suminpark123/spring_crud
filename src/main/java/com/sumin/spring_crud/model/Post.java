898d6ffa013dc56b41469614bc70cffe3c7330cd
suminpark123 <a55026851@gmail.com>
2023-12-25 11:20:08 +0900

    게시판만들기 model 생성

---
+++ src/main/java/com/sumin/spring_crud/model/Post.java
@@ -0,0 +1,30 @@
+package com.sumin.spring_crud.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+    //게시판 타이틀,내용,컨텐츠
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private  String title;
+
+    @Column(name = "description", nullable = false)
+    private  String dsecription;
+
+    @Column(name = "content",nullable = false)
+    private  String content;
+}
--- src/main/java/com/sumin/spring_crud/repository/EmployeeRespository.java
+++
@@ -1,7 +0,0 @@
-package com.sumin.spring_crud.repository;
-
-import com.sumin.spring_crud.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRespository extends JpaRepository<Employee,Long> {
-}
